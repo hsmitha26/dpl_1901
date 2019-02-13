4009779b37788bb117e2672044087472f978a145
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-13 10:46:10 -0700

    adding book.rb

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,14 @@
 class Book
 
-
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
+              
+  def initialize(book_hash)
+    @author_first_name = book_hash[:author_first_name]
+    @author_last_name = book_hash[:author_last_name]
+    @title = book_hash[:title]
+    @publication_date = book_hash[:publication_date]
+  end
 end
